6b1494968dbf778c570944adf7ff3fd0fd5091e8
SanchirHomePC <ksb0891@gmail.com>
2015-07-27 23:33:01 +0300

    init

---
+++ PyQtVS/MainWindow.py
@@ -0,0 +1,89 @@
+﻿from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+import Windows , Objects, Thread
+from collections import defaultdict
+import win32con
+
+class MainWindow(QtWidgets.QWidget):
+
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+
+        self._map = dict()
+
+        self._newKey =''
+
+        self._arrayKeyEvent=[]
+
+        self._rootLayout = QtWidgets.QVBoxLayout(self)
+        self._form = QtWidgets.QFormLayout(self)
+        self._vectorBase = {"One-Click":[29,2],"Create Note":[29,3],"Paste Back":[29,4],"Save File":[29,5]}
+        self._vector=[]
+
+        for category in self._vectorBase:
+            object = Windows.HotKeyObjectView(self,category,self._vectorBase[category])
+            self.onOkAddButton(category,object)
+            
+        self._btn = QtWidgets.QPushButton()
+        self._btn.setText("Add Element")
+        object = Windows.HotKeyObjectView(self, "NewElement", [29,1])
+        self._btn.clicked.connect(lambda: self.onAddButton(object))
+
+        self._rootLayout.addLayout(self._form)
+        self._rootLayout.addWidget(self._btn)
+        self.setLayout(self._rootLayout)
+
+        """self._shorcutThread = Thread.ShortcutThread()
+        self._shorcutThread.start()"""
+
+
+
+    def onOkAddButton(self,category,numberKey):
+        if(self._map.get(category) == None):
+            self._vector.append(str(category))
+
+            self._newKey = numberKey
+            self._newKey._textEdit.setText(numberKey._textEdit.text())
+
+            self._map[category]= numberKey._object
+
+            self._lbl = QtWidgets.QLabel(category)
+            self._form.addRow(self._lbl)
+            self._form.addRow(numberKey)
+        else:
+            self._comboString=''
+
+            for key in numberKey._object._keyValue:
+                if(len(self._comboString)>0):
+                    self._comboString=self._comboString+"+"+str(key)
+                else:
+                    self._comboString=self._comboString+str(key)
+            self._newKey._textEdit.setText(self._comboString)
+            self._map[category]._keyValue=numberKey._object._keyValue
+
+
+    def onAddButton(self,hotKey):
+        print('work')
+        self._newKey = hotKey
+        self._addWindow = Windows.ChangeOrAddHotKeyWindow(hotKey._name)
+        self._addWindow.okAddSignal[str,Windows.HotKeyObjectView].connect(self.onOkAddButton)
+        self._addWindow.show()
+
+    def keyPressEvent(self,event):
+        print(event.nativeScanCode())
+        if self._arrayKeyEvent.count(event.nativeScanCode()) == 0:
+            self._arrayKeyEvent.append(event.nativeScanCode())
+
+    def keyReleaseEvent(self,event):
+        for keyName in self._vectorBase:
+            if self._map[keyName]._keyValue == self._arrayKeyEvent and self._map[keyName]._checkFlag:
+                QMessageBox(QMessageBox.Information,"HotKey Pressed","HotKey "+keyName+" pressed.",QMessageBox.Ok,self).show()
+        self._arrayKeyEvent.clear()
+
+
+    def onSwitchKey(self,info):
+        self._map[info._name]._checkFlag=info._checkAble.isChecked()
+
+
+   
---
+++ PyQtVS/Objects.py
@@ -0,0 +1,80 @@
+﻿from PyQt5 import QtWidgets
+
+keys = {
+    "42": "shift"
+    ,"29": "ctrl"
+    ,"56": "alt"
+    ,"347":"win"
+    ,"328":"up"
+    ,"336":"down"
+    ,"331":"left"
+    ,"333":"right"
+    ,"329":"pgup"
+    ,"337":"pgdown"
+    ,"327":"home"
+    ,"335":"end"
+    ,"338":"insert"
+    ,"28":"enter"
+    ,"15":"tab"
+    ,"57":"space"
+    ,"14":"backspace"
+    ,"339":"del"
+    ,"1":"escape"
+    ,"59": "f1"
+    ,"60":"f2"
+    ,"61":"f3"
+    ,"62":"f4"
+    ,"63":"f5"
+    ,"64":"f6"
+    ,"65":"f7"
+    ,"66":"f8"
+    ,"67":"f9"
+    ,"68":"f10"
+    ,"87":"f11"
+    ,"88":"f12"
+    ,"82":"numpad0"
+    ,"79":"numpad1"
+    ,"80":"numpad2"
+    ,"81":"numpad3"
+    ,"75":"numpad4"
+    ,"76":"numpad5"
+    ,"77":"numpad6"
+    ,"71":"numpad7"
+    ,"72":"numpad8"
+    ,"73":"numpad9"
+}
+
+class HotKeyHandler(QtWidgets.QTextEdit):
+    def __init__(self,title,parent=None):
+        super(HotKeyHandler, self).__init__(parent)
+        self.str=''
+        self.setText(title)
+        self.key=[]
+        self.flagOne=False
+        self.flagTwo=False
+    def keyPressEvent(self,event):
+        if self.key.count(event.nativeScanCode()) == 0:
+            if len(self.key) == 0 and not keys.get(str(event.nativeScanCode())):
+                return 
+            if event.text() !='' or keys.get(str(event.nativeScanCode())):
+                self.key.append(event.nativeScanCode())
+            self.stringAdd=''
+            if keys.get(str(event.nativeScanCode())):
+                self.stringAdd=keys.get(str(event.nativeScanCode()))
+            if len(self.stringAdd) == 0 and event.text() !='':
+                self.stringAdd=event.text()
+            if self.str == '':
+                self.str=self.stringAdd
+            else:
+                self.str=self.str+"+"+self.stringAdd
+        self.setText(self.str)
+        print(self.key)
+
+
+class HotKeyObject():
+    def __init__(self,name ="",number=[1]):
+        self._name = name
+        self._keyValue = number
+        self._checkFlag = False
+          
+
